dfe5d25e93a457a61502dd4ad85d3dfb019a9fc7
naveed <f178149@cfd.nu.edu.pk>
2022-02-23 23:33:49 +0500

    started creating app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DigiClockComponent } from './digi-clock/digi-clock.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DigiClockComponent],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    
+    ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/digi-clock/digi-clock.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-digi-clock',
+  templateUrl: './digi-clock.component.html',
+  styleUrls: ['./digi-clock.component.css'],
+})
+export class DigiClockComponent implements OnInit {
+  private daysArray = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  private date = new Date();
+
+  public hour: any;
+  public minute: any;
+  public second: any;
+  public ampm: any;
+  public day: any;
+  public Date: any;
+  public month: any;
+  public year: any;
+  
+
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      const date = new Date();
+      this.updateDate(date);
+    }, 1000); // this will call the updateDate method in each second
+
+    this.day = this.daysArray[this.date.getDay()];
+    // getDay() returnsthe day in integer formate,from 0 to 6 then takes the corresponding number from the array
+  }
+
+  private updateDate(date: Date) {
+    const hours = date.getHours(); //get the hours from the Date
+
+    this.ampm = hours >= 12 ? 'PM' : 'AM'; // ternry condition which check that time is greater than 12 then condition true hour becoome (PM)
+    this.hour = this.hour < 10 ? '0' + hours : hours; // if the hour is single digit, then add 0 to the hour
+
+    const minutes = date.getMinutes(); //get the minutes from the Date
+    this.minute = minutes < 10 ? '0' + minutes : minutes.toString();
+
+    const second = date.getSeconds(); //get the seconds from the Date
+    this.second = second < 10 ? '0' + second : second.toString();
+
+    const days = date.getDate(); // get the month from the date
+
+    this.Date = days < 10 ? '0' + days : days.toString();
+
+    const month = date.getMonth() + 1; // get the month from the date
+    this.month = month < 10 ? '0' + month : month.toString();
+
+    this.year = date.getFullYear();
+  }
+
+//document.getElementById('digitalTime').innerHTML = 
+}
